3daf5ef1454b00af45dc505a9f151d189f24aaf9
Isaac Escobar <82394696+mrescobar02@users.noreply.github.com>
2025-04-09 18:14:40 -0500

    Revert "Initialize project"

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import type { UserConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-import tailwindcss from "@tailwindcss/vite";
-
-export default {
-  plugins: [react(), tailwindcss()],
-  // ...
-} satisfies UserConfig;
