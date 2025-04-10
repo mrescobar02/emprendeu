bfaebe4d1bc77829a000e7208afec795ff506b15
Isaac Escobar <iescobar@estee.com>
2025-04-10 03:17:31 -0500

    Create Login Page
    Non Interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
-//This is the Test Section for the project
-const App = () => {
-  return <div className="app-container bg-red-500">Hello World</div>;
-};
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Outlet, //Exports the reactDom route to the layout
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Layout from "./components/Layout";
+import AuthLayout from "./components/AuthLayout";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/login" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="home" element={<Home />} />
+        </Route>
+      </>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+}
 
 export default App;
